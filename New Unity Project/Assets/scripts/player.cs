d1820dbe295f873a0e937721c26cbc2b22cc4b02
howard <howard3917@gmail.com>
2020-11-12 20:25:01 +0800

    FINAL

--- New Unity Project/Assets/scripts/player.cs
+++ New Unity Project/Assets/scripts/player.cs
@@ -1,11 +1,51 @@
 ﻿using UnityEngine;
-
-public class player : MonoBehaviour
+using UnityEngine.UI;
+ public class player : MonoBehaviour
 {
 
     public GameObject final;
+
+    public Text textCount;
+
+    public int count;
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("碰到傳送們了");
+
+        if (collision.name == "傳送門")
+        {
+            final.SetActive(true);
+        }
+    
+        if(collision.tag == "櫻桃")
+        {
+            Destroy(collision.gameObject);
+
+            count++;
+
+            textCount.text = "道具數量:" + count;
+
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
 }
